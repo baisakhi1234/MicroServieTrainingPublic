a8f07a3ab9db61ab51b013060761b3b7255e9200
baisakhi1234 <emailbaisakhi@gmail.com>
2020-06-07 11:02:27 +0530

    eureka server

--- src/main/java/com/example/photoappdiscoveryservice/PhotoAppDiscoveryServiceApplication.java
+++ src/main/java/com/example/photoappdiscoveryservice/PhotoAppDiscoveryServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class PhotoappdiscoveryserviceApplication {
+public class PhotoAppDiscoveryServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PhotoappdiscoveryserviceApplication.class, args);
+        SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
     }
 
 }
